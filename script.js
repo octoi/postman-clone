68328bead763b4b58f06cf00d941522bbd6d598b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-05 12:32:31 +0530

    key value pair

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const queryParamsContainer = document.querySelector('[data-query-params]')
+const requestHeadersContainer = document.querySelector('[data-request-headers]')
+const keyValueTemplate = document.querySelector('[data-key-value-template]');
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+	const element = keyValueTemplate.content.cloneNode(true)
+	element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
+		e.target.closest('[data-key-value-pair]').remove()
+	})
+	return element;
+}

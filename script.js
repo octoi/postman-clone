e3d6602fae5460819bb4ba376132458e411bd6ba
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-05 12:43:29 +0530

    key values to object

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
 
+const form = document.querySelector('[data-form]')
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
@@ -16,10 +18,31 @@ document.querySelector('[data-request-headers-btn]').addEventListener('click', (
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	axios({
+		url: document.querySelector('[data-url]').value,
+		method: document.querySelector('[data-method]').value,
+		params: keyValuesToObject(queryParamsContainer),
+		headers: keyValuesToObject(requestHeadersContainer),
+	}).then()
+});
+
 function createKeyValuePair() {
 	const element = keyValueTemplate.content.cloneNode(true)
 	element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
 		e.target.closest('[data-key-value-pair]').remove()
 	})
 	return element;
+}
+
+function keyValuesToObject(container) {
+	const pairs = container.querySelectorAll('[data-key-value-pair]');
+	return [...pairs].reduce((data, pair) => {
+		const key = pair.querySelector(['data-key'])
+		const value = pair.querySelector(['data-value'])
+
+		if (key === '') return data;
+		return { ...data, [key]: value }
+	}, {});
 }
